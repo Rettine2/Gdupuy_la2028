90027e6ef432b30637bec571fd38ef3f996a9630
Gabin Dupuy--Guillemette <gabin.dupuyguillemette@gmail.com>
2026-09-07 15:33:04 +0200

    US1 : modif de athlete.java, DAOAthlete, listerAthlete.jsp

---
+++ src/main/java/sio/la2028/model/Athlete.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.la2028.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author zakina
+ */
+public class
+
+Athlete {
+    
+    private int id;
+    private String prenom ;
+    private String nom ;
+    private LocalDate dateNaissance ;
+    private Pays pays ;
+    public Athlete() {
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Athlete(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Pays getPays() {
+        return pays;
+    }
+
+    public void setPays(Pays pays) {
+        this.pays = pays;
+    }
+    
+    
+}
